72ce344416b3b03d7e3958b63efdd9650475089e
Victor Omonoaa Amakiri <amakiri.dev@gmail.com>
2024-08-18 06:33:18 +0100

    commmiting all files to the repository

---
+++ src/components/Todo.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "../style.css";
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = () => {
+    setTodos((todos) =>
+      todos.concat({
+        text: input,
+        id: generateId(),
+      })
+    );
+    setInput("");
+  };
+
+  const removeTodo = (id) =>
+    setTodos((todos) => todos.filter((t) => t.id !== id));
+  return (
+    <>
+      <div className="container">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="New Todo"
+        />
+
+        <button className="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+        <ol className="todos-list">
+          {todos.map(({ text, id }) => (
+            <li key={id} className="todo">
+              <span>{text}</span>
+              <button className="close" onClick={() => removeTodo(id)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
